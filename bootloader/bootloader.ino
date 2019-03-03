f0802f1b66e8ddaa987652a621a483a949ec754f
Kaan Tasbas <mktasbas@gmail.com>
2019-03-02 20:20:21 -0500

    added read, generic command, and print array functions

--- bootloader/bootloader.ino
+++ bootloader/bootloader.ino
@@ -38,6 +38,72 @@ inline void stm_send(byte cmd)
 //----------------------------------------------------------------
 
 /*
+ * stm_read, blocks until byte recv from STM
+ * return: byte, value recv from STM
+ * param: none
+ */
+inline byte stm_read()
+{
+  while(!STM.available());  // wait to recv byte
+  return STM.read();
+}
+
+//----------------------------------------------------------------
+
+/*
+ * stmCmdGeneric, executes chosen generic command
+ * return: bool, true if successful
+ *         arr, values returned by command
+ *         len, lengh of arr
+ * param: byte   cmd, command code
+ *        byte* arr, array to hold returned values
+ *        int*  len, max length of arr
+ */
+bool stmCmdGeneric(byte cmd, byte* arr, int* len)
+{
+  stm_send(cmd);         // send command
+  stm_send(cmd ^ 0xFF);  // send checksum
+
+  if(stm_read() != 0x79)
+  {
+    // ACKNOWLEDGE FAILED
+    return false;
+  }
+  else
+  {
+    int recv_len = stm_read() + 1;      // number of bytes in response
+    if(recv_len > *len) recv_len = *len;  // limit recv_len to size of arr
+
+    for(int i = 0; i < recv_len; i++)
+    {
+      arr[i] = stm_read();    // load response into arr
+    }
+    
+    *len = recv_len;   // update arr length
+  }
+}
+
+//----------------------------------------------------------------
+
+/*
+ * printArr, prints array to console in HEX format
+ * return: none
+ * param: byte* arr, array to be printed
+ *        int   len, length of array
+ */
+void printArr(byte* arr, int len)
+{
+  for(int i = 0; i < len; i++)
+  {
+    Serial.print(arr[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+}
+
+//----------------------------------------------------------------
+
+/*
  * stm_init, init STM USART connection
  * return: bool, true if successful
  * param: none
@@ -79,5 +145,14 @@ void setup() {
 //----------------------------------------------------------------
 
 void loop() {  
-
+  byte arr[512] = { 0 };
+  int len = 512;
+  
+  // issue Get command
+  if( stmCmdGeneric(0x00, arr, &len) )
+  {
+    printArr(arr, len);
+  }
+  
+  while(1);
 }
